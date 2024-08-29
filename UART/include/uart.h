954562d6a3fa44af741e85e35fd8772cf94be7c9
Dinamiet <iceman.sm39@gmail.com>
2024-08-29 22:02:35 +0200

    Improve UART buffer assignment function naming

--- UART/include/uart.h
+++ UART/include/uart.h
@@ -59,7 +59,7 @@ void UART_Deinit(UART* uart);
  * \param buff Buffer
  * \param size Size of the buffer (bytes)
  */
-void UART_AssignRxBuffer(UART* uart, uint8_t* buff, const size_t size);
+void UART_AssignReceiveBuffer(UART* uart, uint8_t* buff, const size_t size);
 
 /**
  * Assign transmit buffer to the UART
@@ -67,7 +67,7 @@ void UART_AssignRxBuffer(UART* uart, uint8_t* buff, const size_t size);
  * \param buff Buffer
  * \param size Size of the buffer (bytes)
  */
-void UART_AssignTxBuffer(UART* uart, uint8_t* buff, const size_t size);
+void UART_AssignTransmitBuffer(UART* uart, uint8_t* buff, const size_t size);
 
 /**
  * Sets the UART BAUD rate to the specified rate
