4c729a0c2a60d07137c2d488247ef836669698f0
Dinamiet <iceman.sm39@gmail.com>
2024-09-11 21:43:19 +0200

    Timer documentation

--- Timer/include/timer.h
+++ Timer/include/timer.h
@@ -12,40 +12,103 @@
 
 #include "stdint.h"
 
+/**
+ * Timer prescaler divider setup
+ */
 typedef enum _TimerPrescaler_
 {
-	TIMER_DIV_1 = 1,
-	TIMER_DIV_8,
-	TIMER_DIV_32,
-	TIMER_DIV_64,
-	TIMER_DIV_128,
-	TIMER_DIV_256,
-	TIMER_DIV_1024,
+	TIMER_DIV_1 = 1, /** Timer running at system clock */
+	TIMER_DIV_8,     /** Timer running at 1/8 system clock */
+	TIMER_DIV_32,    /** Timer running at 1/32 system clock */
+	TIMER_DIV_64,    /** Timer running at 1/64 system clock */
+	TIMER_DIV_128,   /** Timer running at 1/128 system clock */
+	TIMER_DIV_256,   /** Timer running at 1/256 system clock */
+	TIMER_DIV_1024,  /** Timer running at 1/1024 system clock */
 } TimerPrescaler;
 
+/**
+ * Different timer interrupts
+ *
+ * TODO: Implement other timer interrupts
+ */
 typedef enum _TimerInterruptType_
 {
-	TIMER_INTERRUPT_ROLLOVER = 0x01,
-	/** TODO: Implement other timer interrupts */
+	TIMER_INTERRUPT_ROLLOVER = 0x01, /** Rollover or Overflow interrupt */
 } TimerInterruptType;
 
+/**
+ * Available hardware Timer instances
+ *
+ * TODO: Implement other timers
+ */
 typedef enum _TimerInstance_
 {
 	TIMER0,
-	/** TODO: Implement other timers */
 } TimerInstance;
 
+/**
+ * Interrupt handler for timer interrupts
+ */
 typedef void (*TimerInterrupt)(void);
 
+/**
+ * Timer
+ */
 typedef struct _Timer_ Timer;
 
-Timer*   Timer_GetInstance(const TimerInstance instance);
-void     Timer_Init(Timer* timer);
-void     Timer_Deinit(Timer* timer);
-void     Timer_Configure(Timer* timer, const TimerPrescaler prescaler);
-void     Timer_EnableInterrupt(Timer* timer, const TimerInterruptType type, const TimerInterrupt handler);
-void     Timer_Start(Timer* timer);
-void     Timer_Stop(Timer* timer);
+/**
+ * Retreives the provided hardware Timer instance
+ * \param instance The Timer instance to return
+ * \return Reference to Timer instance
+ */
+Timer* Timer_GetInstance(const TimerInstance instance);
+
+/**
+ * Initialize and power-on Timer hardware
+ * The Timer will be initialized in an off/disabled state
+ * \param timer Timer to initialize
+ */
+void Timer_Init(Timer* timer);
+
+/**
+ * Deinitialize and power-off Timer hardware
+ * The Timer will be stopped before deinitialization
+ * \param timer Timer to deinitialize
+ */
+void Timer_Deinit(Timer* timer);
+
+/**
+ * Configures timer parameters
+ * \param timer The timer to configure
+ * \param prescaler The prescale value the timer should use
+ */
+void Timer_Configure(Timer* timer, const TimerPrescaler prescaler);
+
+/**
+ * Enable a certain timer interrupt
+ * \param timer The timer which interrupt should be enabled
+ * \param type The type of interrupt to enable
+ * \param handler The function that will be called when the interrupt occurs
+ */
+void Timer_EnableInterrupt(Timer* timer, const TimerInterruptType type, const TimerInterrupt handler);
+
+/**
+ * Start timer
+ * \param timer The timer to start
+ */
+void Timer_Start(Timer* timer);
+
+/**
+ * Stop timer
+ * \param timer The timer to stop
+ */
+void Timer_Stop(Timer* timer);
+
+/**
+ * Reads the current counter value of the timer
+ * \param timer Timer's current value to retrieve
+ * \return The current timer value
+ */
 uint16_t Timer_GetValue(const Timer* timer);
 
 #endif
